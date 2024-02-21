da0a99edb7cbe51bdb3417989633245073508425
Merlinlibin <merlinlibinmerlin@gmail.com>
2024-02-21 21:01:42 +0530

    minor animation

--- src/components/AnimatedPage.jsx
+++ src/components/AnimatedPage.jsx
@@ -3,9 +3,9 @@ import { motion } from "framer-motion";
 
 function AnimatedPage({ children }) {
   const animation = {
-    initial: { opacity: 0, x: 100 },
-    animate: { opacity: 1, x: 0 },
-    exit: { opacity: 0, x: 100 },
+    initial: { opacity: 0, x: 200 },
+    animate: { opacity: 1, x: 0,  },
+    exit: { opacity: 0, x: -200 },
   };
   return (
     <motion.div
@@ -13,7 +13,7 @@ function AnimatedPage({ children }) {
       initial="initial"
       animate="animate"
       exit="exit"
-      transition={{ duration: 1 }}>
+      transition={{ duration: 2 }}>
       {children}
     </motion.div>
   );
